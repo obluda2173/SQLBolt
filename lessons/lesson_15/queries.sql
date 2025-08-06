499dac945702a285cedaa1bc0e589b2786601449
obluda2173 <obluda2173@gmail.com>
2025-08-06 16:25:01 +0200

    lesson 15

---
+++ lessons/lesson_15/queries.sql
@@ -0,0 +1,11 @@
+-- This database is getting too big, lets remove all movies that were released before 2005.
+-- DELETE FROM movies
+-- WHERE year < 2005;
+
+-- SELECT * FROM movies;
+
+-- Andrew Stanton has also left the studio, so please remove all movies directed by him.
+-- DELETE FROM movies
+-- WHERE director LIKE "andrew stanton";
+
+-- SELECT title, director, year FROM movies;
