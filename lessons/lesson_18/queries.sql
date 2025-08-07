b6a5413f0b2ab0443c503351d373469adb63d2b7
obluda2173 <obluda2173@gmail.com>
2025-08-07 17:38:02 +0200

    lesson 18

---
+++ lessons/lesson_18/queries.sql
@@ -0,0 +1,5 @@
+-- We've sadly reached the end of our lessons, lets clean up by removing the Movies tableI
+-- DROP TABLE IF EXISTS movies;
+
+-- And drop the BoxOffice table as well
+-- DROP TABLE IF EXISTS boxoffice;
